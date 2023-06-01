f55380b73737f273fef8be13b75cb04622d4fcc0
Alina Prish <alinaprish92@mail.ru>
2023-06-01 14:33:39 +0300

    revision

--- task.js
+++ task.js
@@ -1,28 +1,31 @@
 "use strict"
 function solveEquation(a, b, c) {
+  let x1, x2;
   let arr = [];
-  let arr1 = [x1, x2];
-  let arr2 = [x1];
-  if(a==0) 
-  return "Ошибка, a=0 недопустимое значение!";
-  var D=b*b-4*a*c;
-  var x1, x2;
+  //let arr1 = [x1, x2];
+  //let arr2 = [x1];
+  if(a==0) {
+    return "Ошибка, a=0 недопустимое значение!";
+  }
+  let D=b*b-4*a*c;
   if(D>0) {
     x1=(-b+Math.sqrt(D))/(2*a);
     x2=(-b-Math.sqrt(D))/(2*a);
-  return arr1;
+  return arr[x1, x2];
 } else if(D==0) {
   x1=-b/(2*a);
-  return arr2;
-} else if(D<0) {
+  return arr[x1];
+}
   return arr;
 }
-}
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   let S = amount-contribution;
-  let pay = S*(percent/100/12+(percent/100/12/(((1+percent/100/12)**countMonths)-1)));
-  let sum = pay*countMonths;
-  return sum;
+  let P = percent/100/12;
+  let pay = S*(P+(P(((1+P)**countMonths)-1)));
+  let sum = (pay*countMonths).toFixed(2);
+  let sum2 = Number.parseInt(sum);
+  return sum2
 }
-  
+   
