f59d4a5df17c38a73598eeaa2bdc43791a8cc386
Alina Prish <alinaprish92@mail.ru>
2023-06-29 18:58:08 +0300

    add revision arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,13 @@
 function compareArrays(arr1, arr2) {
-  const result = (arr1, arr2) => arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
-  return result;
+  return result = (arr1, arr2) => arr1.length === arr2.length && arr1.every((number, index) => number === arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  let result = arr.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users)=> {
+  return result = arr.filter(user => user.gender === gender).map(user => user.age).reduce((acc, item, index, users)=> {
     acc+=item;
     if(index === users.length - 1){
         return acc / users.length;
     }
     return acc;
   }, 0)
-return result;
 }
